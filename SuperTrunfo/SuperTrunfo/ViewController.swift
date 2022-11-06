1a0fcb5bcc4f6beef067b5ebe870297920e44234
ViniciusCleim <vinicius.cleim@gmail.com>
2022-11-06 15:50:00 -0300

    first commit

---
+++ SuperTrunfo/SuperTrunfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperTrunfo
+//
+//  Created by Vinicius Cleim on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
